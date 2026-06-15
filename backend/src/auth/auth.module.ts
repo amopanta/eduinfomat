e4e3451997103541cb37501f1a3d9eefca91312c
Pedro Manjarres <122238970+amopanta@users.noreply.github.com>
2026-06-14 20:30:13 -0500

    feat(auth): import audit module

--- backend/src/auth/auth.module.ts
+++ backend/src/auth/auth.module.ts
@@ -1,12 +1,14 @@
 import { Module } from '@nestjs/common';
 import { JwtModule } from '@nestjs/jwt';
+import { AuditModule } from '../audit/audit.module';
+import { UsersModule } from '../users/users.module';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
-import { UsersModule } from '../users/users.module';
 
 @Module({
   imports: [
     UsersModule,
+    AuditModule,
     JwtModule.register({
       secret: process.env.JWT_SECRET || 'dev_secret',
       signOptions: { expiresIn: '1h' }
