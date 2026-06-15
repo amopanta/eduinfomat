277d997be78ac24d72bfd95bbc974160a458bf64
Pedro Manjarres <122238970+amopanta@users.noreply.github.com>
2026-06-14 20:13:13 -0500

    feat(rbac): register roles controller

--- backend/src/rbac/rbac.module.ts
+++ backend/src/rbac/rbac.module.ts
@@ -1,7 +1,11 @@
 import { Module } from '@nestjs/common';
+import { DatabaseModule } from '../database/database.module';
+import { RbacController } from './rbac.controller';
 import { RbacService } from './rbac.service';
 
 @Module({
+  imports: [DatabaseModule],
+  controllers: [RbacController],
   providers: [RbacService],
   exports: [RbacService]
 })
