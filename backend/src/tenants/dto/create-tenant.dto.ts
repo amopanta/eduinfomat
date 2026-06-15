1d85d836284d7fceb69481e577462261239d1ab9
Pedro Manjarres <122238970+amopanta@users.noreply.github.com>
2026-06-14 20:08:41 -0500

    feat(tenants): add create tenant dto

---
+++ backend/src/tenants/dto/create-tenant.dto.ts
@@ -0,0 +1,12 @@
+import { IsString } from 'class-validator';
+
+export class CreateTenantDto {
+  @IsString()
+  name!: string;
+
+  @IsString()
+  code!: string;
+
+  @IsString()
+  type!: string;
+}
