d83e72041324c6c136e780a017f0ae522f96df41
Pedro Manjarres <122238970+amopanta@users.noreply.github.com>
2026-06-14 20:09:29 -0500

    feat(tenants): expose crud endpoints

--- backend/src/tenants/tenants.controller.ts
+++ backend/src/tenants/tenants.controller.ts
@@ -1,12 +1,34 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Post, Put } from '@nestjs/common';
+import { CreateTenantDto } from './dto/create-tenant.dto';
+import { UpdateTenantDto } from './dto/update-tenant.dto';
 import { TenantsService } from './tenants.service';
 
 @Controller('tenants')
 export class TenantsController {
-  constructor(private readonly tenantsService: TenantsService) {}
+  constructor(private readonly service: TenantsService) {}
 
   @Get()
   list() {
-    return this.tenantsService.list();
+    return this.service.list();
+  }
+
+  @Get(':id')
+  get(@Param('id') id: string) {
+    return this.service.get(id);
+  }
+
+  @Post()
+  create(@Body() dto: CreateTenantDto) {
+    return this.service.create(dto);
+  }
+
+  @Put(':id')
+  update(@Param('id') id: string, @Body() dto: UpdateTenantDto) {
+    return this.service.update(id, dto);
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return this.service.deactivate(id);
   }
 }
