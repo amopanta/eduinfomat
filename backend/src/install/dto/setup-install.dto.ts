040e8d89de0c3965ac1b8db4f5b22accd25a1e55
Pedro Manjarres <122238970+amopanta@users.noreply.github.com>
2026-06-15 20:50:04 -0500

    feat(install): add setup install dto

---
+++ backend/src/install/dto/setup-install.dto.ts
@@ -0,0 +1,8 @@
+export class SetupInstallDto {
+  tenant_name!: string;
+  tenant_code!: string;
+  admin_email!: string;
+  admin_password!: string;
+  admin_first_name!: string;
+  admin_last_name!: string;
+}
